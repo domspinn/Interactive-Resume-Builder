7e90fe3aa267217db896eaf12a61415ea6a08701
Dominic-PC <domospinn21@gmail.com>
2024-12-07 16:15:32 -0500

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import EditorPage from './pages/EditorPage';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
       <div className="min-h-screen flex flex-col">
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/editor" element={<EditorPage />} />
